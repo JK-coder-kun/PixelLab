219bc9991e8ef822f538c40ec45119cd69bfda88
JK-coder-kun <JK-coder-kun@github.com>
2024-02-25 16:58:50 +0630

    remove test files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
-//#include <stdio.h>
-//#include "glad/glad.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "shader/shader_s.h"
@@ -29,32 +28,31 @@ Texture texture;
 
 float vertices[] = {
         // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
 };
 
-unsigned int VBO, VAO, EBO;
-
-//for debug
-string texWidth,texHeight;
-string texdimension;
+float quadVertices[] = {
+       // positions          // colors           // texture coords
+         0.6f,  0.6f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.6f, -0.6f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.6f, -0.6f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.6f,  0.6f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
 
 
 int main()
 {
-    //openFileDialog();
     // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "PIXEL LAB", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Pixel Lab", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -67,12 +65,10 @@ int main()
 
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
     glewInit();
 
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
     ImGui_ImplGlfwGL3_Init(window, true);
 
     // Setup style
@@ -84,20 +80,16 @@ int main()
     static float pixelBlocks = 0.0f;
     ImVec4 clear_color = ImVec4(0.2f, 0.3f, 0.3f, 1.0f);
 
-    //string imgPath;
-    //bool textureActive, newTextureSet;
-    //Texture texture;
 
-
-    // build and compile our shader zprogram
-    // ------------------------------------
-    Shader ourShader("./shader/test.vert", "./shader/shader.frag");
+    // build and compile our shader program
+    Shader ourShader("./shader/shader.vert", "./shader/shader.frag");
 
 
     unsigned int indices[] = {
         0, 1, 3, // first triangle
         1, 2, 3  // second triangle
     };
+
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -121,22 +113,79 @@ int main()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
+    //ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+
+    unsigned int fbo,fbo_texture;
+    glGenFramebuffers(1,&fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER,fbo);
+    glGenTextures(1, &fbo_texture);
+    glBindTexture(GL_TEXTURE_2D, fbo_texture);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, windowWidth, windowHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL); // Create a standard texture with the width and height of our window
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo_texture, 0); // Attach the texture fbo_texture to the color buffer in our frame buffer
+    glBindFramebuffer(GL_FRAMEBUFFER,0);
 
 
-    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-    // -------------------------------------------------------------------------------------------
-    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-    //ourShader.setFloat("intensity", intensity);
+     unsigned int quadVAO, quadVBO,quadEBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glGenBuffers(1, &quadEBO);
 
+    glBindVertexArray(quadVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
-        ImGui_ImplGlfwGL3_NewFrame();
+        // input
+        processInput(window);
 
-        // 1. Show a simple window.
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
+        //created framebuffer object
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        glViewport(0,0,texture.GetWidth(),texture.GetHeight());
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        if(textureActive)texture.Bind();
+
+        // render container
+        ourShader.use();
+        ourShader.setFloat("greyIntensity", greyIntensity);
+        ourShader.setFloat("pixelBlocks", pixelBlocks);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // default framebuffer object
+        glBindFramebuffer(GL_FRAMEBUFFER,0);
+        glViewport(0,0,windowWidth,windowHeight);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //GUI window
+        ImGui_ImplGlfwGL3_NewFrame();
         {   ImGui::Begin("Menu");
             ImGui::SetWindowPos(ImVec2(10,10),ImGuiCond_FirstUseEver);
             ImGui::Text("Welcome to Pixel Lab!");                           // Display some text (you can use a format string too)
@@ -158,9 +207,11 @@ int main()
                 if(saveFileDialog(szFile))
                 {
                     string path = convertToString(szFile, MAX_PATH) + ".png";
-                    bool success = Texture::SaveAsImage(path,vertices[24],vertices[25],texture.GetWidth(), texture.GetHeight());
+                    glBindFramebuffer(GL_FRAMEBUFFER,fbo);
+                    bool success = Texture::SaveAsImage(path,0,0,texture.GetWidth(), texture.GetHeight());
                     if(!success) std::cout<<"Unable to save image"<<std::endl;
                     else std::cout<<"Image has been saved!"<<std::endl;
+                    glBindFramebuffer(GL_FRAMEBUFFER,0);
                 }else std::cout<<"No path was given to save!"<<std::endl;
             }
 
@@ -170,77 +221,79 @@ int main()
             ImGui::End();
         }
 
-        //load and create a texture
+        //load image and create a texture
         if(newTextureSet)
         {
             imgPath = openFileDialog();
             texture.LoadTexture(imgPath);
 
             setRenderCoordinate(texture.GetWidth(), texture.GetHeight());
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
             newTextureSet = false;
-        }
 
+            //regenerate fbo to renew width and height of the texture that is attached to fbo
+            glDeleteFramebuffers(1,&fbo);
+            glDeleteTextures(1,&fbo_texture);
+
+            glGenFramebuffers(1,&fbo);
+            glBindFramebuffer(GL_FRAMEBUFFER,fbo);
+
+            glGenTextures(1, &fbo_texture); // Generate one texture
+            glBindTexture(GL_TEXTURE_2D, fbo_texture); // Bind the texture fbo_texture
+
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.GetWidth(), texture.GetHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL); // Create a standard texture with the width and height of our window
+
+            // Setup the basic texture parameters
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo_texture, 0);
+            glBindFramebuffer(GL_FRAMEBUFFER,0);
+        }
 
         if(textureActive && windowResize)
         {
             setRenderCoordinate(texture.GetWidth(), texture.GetHeight());
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
             windowResize = false;
         }
 
-        // input
-        // -----
-        processInput(window);
 
-        // render
-        // ------
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        if(textureActive)
-        {
-            // bind textures on corresponding texture units
-            texture.Bind();
-        }
-
-
-        // render container
-        ourShader.use();
-        ourShader.setFloat("greyIntensity", greyIntensity);
-        ourShader.setFloat("pixelBlocks", pixelBlocks);
-
-        glBindVertexArray(VAO);
-
-
+        //container to render our created fbo
+        glBindVertexArray(quadVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, fbo_texture);	// use the color attachment texture as the texture of the quad plane
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         ImGui::Render();
         ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
+    glDeleteFramebuffers(1,&fbo);
+    glDeleteVertexArrays(1,&quadVAO);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &quadVBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &quadEBO);
 
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
     // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-//set coordinate to match image ratio
+//set quad coordinate to match image ratio
 void setRenderCoordinate(int width, int height)
 {
     float initialCoord[] = {
@@ -250,59 +303,50 @@ void setRenderCoordinate(int width, int height)
         -0.6f, -0.6f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
         -0.6f,  0.6f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
-    std::copy(initialCoord, initialCoord+sizeof(initialCoord)/sizeof(initialCoord[0]), vertices);
+    std::copy(initialCoord, initialCoord+sizeof(initialCoord)/sizeof(initialCoord[0]), quadVertices);
+
+    //if image width is greater, set quad width default and adjust height
     if(width >= height)
     {
         float winHWRatio = (float)windowWidth/(float)windowHeight;
         float hwRatio = (float)1.2f * winHWRatio* ((float)height/(float)width);
         std::cout<<"WinHW: "<<winHWRatio<<std::endl;
         std::cout<<"width: "<<width<<", height: "<<height<<", hwRatio: "<<hwRatio/1.2f<<std::endl;
-        vertices[1]=0 + hwRatio/2;
-        vertices[25]=vertices[1];
-        vertices[9]= 0 - hwRatio/2;
-        vertices[17] = vertices[9];
-        std::cout<<"width: "<<(vertices[0]-vertices[16])<<", height: "<<(vertices[1]-vertices[9])<<", hwRatio: "<<(vertices[1]-vertices[9])/(vertices[0]-vertices[16])<<std::endl;
+        quadVertices[1]=0 + hwRatio/2;
+        quadVertices[25]=quadVertices[1];
+        quadVertices[9]= 0 - hwRatio/2;
+        quadVertices[17] = quadVertices[9];
+        std::cout<<"width: "<<(quadVertices[0]-quadVertices[16])<<", height: "<<(quadVertices[1]-quadVertices[9])<<", hwRatio: "<<(quadVertices[1]-quadVertices[9])/(quadVertices[0]-quadVertices[16])<<std::endl;
 
 
-    }else
+    }else  //if image height is greater, set quad height default and adjust width
     {
         float winWHRatio = (float)windowHeight/(float)windowWidth;
         float whRatio = (float)1.2f * winWHRatio*((float)width/(float)height);
         std::cout<<"WinWH: "<<winWHRatio<<std::endl;
         std::cout<<"width: "<<width<<", height: "<<height<<", whRatio: "<<whRatio/1.2f<<std::endl;
-        vertices[0] = 0 + whRatio/2;
-        vertices[8] = vertices[0];
-        vertices[16]= 0 - whRatio/2;
-        vertices[24]= vertices[16];
-        std::cout<<"width: "<<(vertices[0]-vertices[16])<<", height: "<<(vertices[1]-vertices[9])<<", whRatio: "<<(vertices[0]-vertices[16])/(vertices[1]-vertices[9])<<std::endl;
+        quadVertices[0] = 0 + whRatio/2;
+        quadVertices[8] = quadVertices[0];
+        quadVertices[16]= 0 - whRatio/2;
+        quadVertices[24]= quadVertices[16];
+        std::cout<<"width: "<<(quadVertices[0]-quadVertices[16])<<", height: "<<(quadVertices[1]-quadVertices[9])<<", whRatio: "<<(quadVertices[0]-quadVertices[16])/(quadVertices[1]-quadVertices[9])<<std::endl;
 
     }
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
-        char szFile[MAX_PATH] = {0};
-        if(saveFileDialog(szFile))
-        {
-            string path = convertToString(szFile, MAX_PATH) + ".png";
-            bool success = true;//Texture::SaveAsImage(path,window);
-            if(!success) std::cout<<"Unable to save image"<<std::endl;
-            else std::cout<<"Image has been saved!"<<std::endl;
-        }else std::cout<<"No path was given to save!"<<std::endl;
     }
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+// glfw: whenever the window size changed, this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
+    // make sure the viewport matches the new window dimensions
     glViewport(0, 0, width, height);
     windowWidth = width;
     windowHeight = height;
